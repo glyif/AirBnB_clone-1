724be74aabdf2799613fa69263c3934ae89fe79a
glyif <122@holbertonschool.com>
2017-08-11 16:07:26 -0700

    DBStorage class creation
    Initial creation of DBStorage class, not finished, initial commit

---
+++ models/engine/db_storage.py
@@ -0,0 +1,51 @@
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.base_model import Base
+from models import CNC
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{:s}:{:s}@localhost/{:s}'.
+                                      format(getenv('HBNB_MYSQL_USER'),
+                                             getenv('HBNB_MYSQL_PWD'),
+                                             getenv('HBNB_MYSQL_HOST'),
+                                             getenv('HBNB_MYSQL_DB')))
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(self.__engine)
+        self.__session = session()
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        query_data = {}
+
+        if cls is None:
+            for valid_class in CNC.keys():
+                for instance in self.__session.query(valid_class):
+                    query_data.update({instance.id: instance})
+            return query_data
+        else:
+            for instance in self.__session.query(cls):
+                query_data.update({instance.id: instance})
+            return query_data
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        # self.__session(create_engine()) something like that, not enough time to finish
+
