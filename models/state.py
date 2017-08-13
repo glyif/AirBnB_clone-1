28131df71877ccb7ea263ca9743c4554376f6259
glyif <122@holbertonschool.com>
2017-08-12 17:15:28 -0700

    Created initial state sqlalchemy
    initial State model sqlalchemy model

--- models/state.py
+++ models/state.py
@@ -3,13 +3,22 @@
 State Class from Models Module
 """
 
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """State class handles all application states"""
 
-    name = ''
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete-orphan",  backref="State")
+    else:
+        name = ''
 
     def __init__(self, *args, **kwargs):
         """instantiates a new state"""
