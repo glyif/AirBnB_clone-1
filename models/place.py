09d332910f347d16584dbb3596b92980bc6cb147
glyif <122@holbertonschool.com>
2017-08-15 18:26:33 -0700

    added relationship between place_amenities and Amentiy

--- models/place.py
+++ models/place.py
@@ -13,7 +13,10 @@ from models.base_model import BaseModel, Base
 if getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60), ForeignKey('places.id')),
-                          Column('amenity_id', String(60), ForeignKey('amenities.id')))
+                          Column('amenity_id', String(60), ForeignKey('amenities.id')),
+                          relationship("Amenity",
+                                       cascade="all, delete",
+                                       backref="amenities"))
 
 
 class Place(BaseModel, Base):
@@ -25,7 +28,7 @@ class Place(BaseModel, Base):
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024))
-        number_rooms = Column(Integer, nullable=False, default=0)
+        number_rooms = Column(Integer, nulble=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
