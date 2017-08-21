1f97eefad449b1160986a56b2b50e1ce09c06baa
glyif <122@holbertonschool.com>
2017-08-21 10:25:03 -0700

    changed wget urls for index and config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,11 @@
 # sets up server for deployment
 
 sudo mkdir -p /data/web_static/releases/test/
-wget
+wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/deploy/test_index.html
 sudo mv test_index.html /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu /data -R
-wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/config
+wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/deploy/config
 sudo mv config /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx restart
