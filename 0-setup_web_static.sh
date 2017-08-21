073281f11eff17db2d9d5cd6851283b3e39968cd
glyif <122@holbertonschool.com>
2017-08-21 09:52:45 -0700

    nginx config with deployment server setup script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# sets up server for deployment
+
+mkdir -p /data/web_static/releases/test/
+echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown ubuntu:ubuntu /data -R
+wget
+sudo cp config /etc/nginx/sites-available/default
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enable/default
+sudo service nginx restart
