ce876a86d943933e8d93c5bb83634f2ac706924d
glyif <122@holbertonschool.com>
2017-08-21 10:06:46 -0700

    updated with wget url for config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ mkdir -p /data/web_static/releases/test/
 echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu /data -R
-wget
+wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/config
 sudo cp config /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enable/default
 sudo service nginx restart
