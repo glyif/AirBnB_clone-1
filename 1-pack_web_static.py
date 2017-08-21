9cc3f2110d38064fec41e1eb201df234dcfe475f
Bobby Yang <122@holbertonschool.com>
2017-08-21 19:31:23 +0000

    pep8 #1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,19 +9,20 @@ from os import path, makedirs
 
 from fabric import operations
 
+
 def do_pack():
     """
     packs static folder
     """
     try:
-        archive_name = "web_static_" + str(datetime.now().strftime("%Y%m%d%H%M%S"))
+        archive_name = "web_static_" + str(datetime.now().
+                                           strftime("%Y%m%d%H%M%S"))
 
         makedirs("./versions", exist_ok=True)
 
-        operations.local("tar -cvzf versions/{}.tgz web_static/".format(archive_name))
+        operations.local("tar -cvzf versions/{}.tgz web_static/".
+                         format(archive_name))
 
         return "versions/{}".format(archive_name)
     except:
         return None
-
-
