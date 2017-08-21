c397032904c3527a5244636e5db8746532906803
glyif <122@holbertonschool.com>
2017-08-21 10:23:48 -0700

    added index.html, sudoed commands, moved config into deploy folder

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # sets up server for deployment
 
-mkdir -p /data/web_static/releases/test/
-echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+wget
+sudo mv test_index.html /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu /data -R
 wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/config
-sudo cp config /etc/nginx/sites-available/default
+sudo mv config /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx restart
